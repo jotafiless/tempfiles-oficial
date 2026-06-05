6809caeda01f5c0cc321374dad792ac0b1893b72
Briceño <javier.briceno07@inacapmail.cl>
2026-06-04 23:19:06 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // Inicialización del Cliente de Supabase para "Temp Files"
 
 const SUPABASE_URL = "https://deegzgbeephyrgulirjj.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZWd6Z2JlZXBoeXJndWxpcmpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MTI3MzMsImV4cCI6MjA5NjE4ODczM30.6QXJMMU8iTC7komea40K50zCK8hg25M7JpG2X_ky_D0";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZWd6Z2JlZXBoeXJndWxpcmpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MTI3MzMsImV4cCI6MjA5NjE4ODczM30.6QXJMMU8iTC7komea40K50zCK8hg25M7JpG2X_ky_D0";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
